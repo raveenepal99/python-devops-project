8dab2619bcec2e733e1d34d68af976f6f0cab4cd
raveenepal99 <140954224+raveenepal99@users.noreply.github.com>
2024-10-09 10:34:37 +1100

    update home page message and corresponding test

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+import os
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+import logging
 
 app = Flask(__name__)
 
+# Instrumentation Key from environment variable
+instrumentation_key = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY')
+
+# Setup logging
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(connection_string=f'InstrumentationKey={instrumentation_key}'))
+
+# Setup tracing
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string=f'InstrumentationKey={instrumentation_key}'),
+    sampler=ProbabilitySampler(rate=1.0)
+)
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    logger.warning('Home page accessed')
+    return 'Hello, DevOps Pipeline! This is an updated message.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
