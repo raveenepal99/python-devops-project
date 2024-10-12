20e7a7ba085a7ff167cbee28394e36f7f6b572fd
raveenepal99 <140954224+raveenepal99@users.noreply.github.com>
2024-10-13 10:20:36 +1100

    simplified app

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask
-import logging
 
 app = Flask(__name__)
 
-# Set up basic logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 @app.route('/')
 def home():
-    logger.info("Home page accessed")
     return "Hello, DevOps Pipeline! This is a simplified message."
 
 if __name__ == '__main__':
