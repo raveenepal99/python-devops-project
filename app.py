1604075520c82cabdf3c49a21e1fc408f1b48011
raveenepal99 <140954224+raveenepal99@users.noreply.github.com>
2024-10-13 10:29:41 +1100

    updated port

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello, DevOps Pipeline! This is a simplified message."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8000)
